417e41e0c000cbf44d833c20235d59ebb0f60454
BinhDo960 <57016565+BinhDo960@users.noreply.github.com>
2019-11-25 14:28:59 -0700

    Create manual.m

---
+++ manual.m
@@ -0,0 +1,54 @@
+global key
+InitKeyboard();
+
+while 1
+    pause(0.1);
+    switch key
+        case 'uparrow' 
+            disp('Up Arrow Pressed!');
+            brick.MoveMotor('A', -50);
+            brick.MoveMotor('A', -51);
+            pause (.25);
+            brick.StopMotor('AB');
+            
+        case 'downarrow'
+            disp('Down Arrow Pressed!'); 
+            brick.MoveMotor('A', 50);
+            brick.MoveMotor('A', 51);
+            pause (.25);
+            brick.StopMotor('AB');
+            
+        case 'leftarrow'
+            disp('Left Arrow Pressed!');
+            brick.MoveMotor('A', -50);
+            pause (.25);
+            brick.StopMotor('A');
+            
+        case 'rightarrow'
+            disp('Right Arrow Pressed!'); 
+            brick.MoveMotor('B', -50);
+            pause (.25);
+            brick.StopMotor('B');
+            
+        case 'l'
+            disp('l Key Pressed!');
+            brick.MoveMotor('C', 15);
+            pause (.25);
+            brick.StopMotor('C');
+            
+        case 'd'
+            disp('d Key Pressed!');
+            brick.MoveMotor('C', -8);
+            pause (.25);
+            brick.StopMotor('C');
+            
+        case 0  
+            disp('No Key Pressed!'); 
+            brick.StopAllMotors;
+            
+        case 'q'    
+            break; 
+    end
+    
+end
+CloseKeyboard(); 
